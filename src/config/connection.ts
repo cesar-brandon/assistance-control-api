f7168bade2115acebe87a87b2c887bf9fb343345
Cesar Brandon <cesarbrandon000@gmail.com>
2023-01-23 14:13:07 -0500

    fix(connection):modify connection

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -4,10 +4,11 @@ import { Sequelize } from "sequelize";
 const database = <string>process.env.DATABASE;
 const password = <string>process.env.PASSWORD;
 const username = <string>process.env.USER;
+const host = <string>process.env.HOST;
+const port = <number>parseInt(<string>process.env.PORT);
 
-const connection = new Sequelize(database, username, password, {
-  host: "localhost",
-  dialect: "mysql",
-});
+const connection = new Sequelize(
+  `mysql://${username}:${password}@${host}:${port}/${database}`
+);
 
 export default connection;
